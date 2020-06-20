a797617d9fcd77056dd3d639d9a734b2fbbbe29c
Stanislav Shurupin <stanislav.shurupin@gmail.com>
2020-06-20 18:25:40 +0300

    Updates

---
+++ precache-manifest.08b70ce15659405f54969fdf6a66efe9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efce9e228979075eadd9b934452fdf2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01a0b0c48e0d140f23b",
+    "url": "/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "c5dd00e36b20446247d6",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "bc068ef426157d15ae7c",
+    "url": "/static/css/main.4e708ecf.chunk.css"
+  },
+  {
+    "revision": "b1e07904ded3490c7086",
+    "url": "/static/js/2.079d63f6.chunk.js"
+  },
+  {
+    "revision": "a01a0b0c48e0d140f23b",
+    "url": "/static/js/3.fed959ee.chunk.js"
+  },
+  {
+    "revision": "c5dd00e36b20446247d6",
+    "url": "/static/js/4.1faa55dc.chunk.js"
+  },
+  {
+    "revision": "bc068ef426157d15ae7c",
+    "url": "/static/js/main.a027699c.chunk.js"
+  },
+  {
+    "revision": "002f804dc8fcabeba909",
+    "url": "/static/js/runtime-main.dc6efe98.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e47c3e26372c670c4a4eeac6eea0bf98",
+    "url": "/static/media/airbnbLogo.e47c3e26.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
